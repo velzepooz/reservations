1400602613d8c97075d2f9a95579151e9443f5ec
glebrulev <gleb@golaunchlabs.com>
2024-06-17 11:14:32 +0200

    fix migration

--- src/migrations/1718216858853-add-base-entities.ts
+++ src/migrations/1718216858853-add-base-entities.ts
@@ -12,9 +12,9 @@ export class AddBaseEntities1718216858853 implements MigrationInterface {
     );
     await queryRunner.query(
       `CREATE TABLE "reservation" ("id" SERIAL NOT NULL, "amenityId" integer NOT NULL, "userId" integer NOT NULL, "startTime" integer NOT NULL, "endTime" integer NOT NULL, "date" bigint NOT NULL, CONSTRAINT "CHK_763323b02de7f4b6aa2e21cd90" CHECK ("date" >= 0), CONSTRAINT "CHK_901626ce00b16fbb7b42ba27a5" CHECK ("endTime" > "startTime"), CONSTRAINT "CHK_85afe8b662f8db992ce65df100" CHECK ("endTime" < 1440), CONSTRAINT "CHK_6d38b706445af26bd87a174517" CHECK ("endTime" >= 0), CONSTRAINT "CHK_ed5c941273790ec3efe35c41f0" CHECK ("startTime" < 1440), CONSTRAINT "CHK_985e293e70567dd71c797452a6" CHECK ("startTime" >= 0), CONSTRAINT "PK_48b1f9922368359ab88e8bfa525" PRIMARY KEY ("id"))`,
-      await queryRunner.query(
-        `CREATE INDEX "idx_date" ON "reservation" ("date") `,
-      ),
+    );
+    await queryRunner.query(
+      `CREATE INDEX "idx_date" ON "reservation" ("date") `,
     );
     await queryRunner.query(
       `ALTER TABLE "reservation" ADD CONSTRAINT "FK_529dceb01ef681127fef04d755d" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
